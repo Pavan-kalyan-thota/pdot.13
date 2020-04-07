70d73a9e8bec7cc7e8ee7db79dfd453519865408
Nithin_kumar_Reddy_Magatala <nithin.magatala@gmail.com>
2020-04-07 22:54:04 +0530

    Create Problem_2.java

---
+++ problem_solving/java/Problem_2.java
@@ -0,0 +1,31 @@
+class Rotate
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int shifts=sc.nextInt();
+    int[] arr=new arr[size];
+    for(int i=0;i<size;++i)
+    {
+      arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<shifts;i++)
+    {
+      int j,firstElement;
+      firstElement=arr[0];
+      for(j=0;j<arr.length-1;j++)
+      {
+        arr[j]=arr[j+1];
+      }
+    arr[j]=firstElement;
+    }
+    
+    for(int i=0;i<size;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
+    
+    
