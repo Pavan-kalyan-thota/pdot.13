c42cf4dc0372ee48dd2ab679cdc151915d1c2d6e
Ravula Nishanth <nishanth.ravula@outlook.com>
2020-04-07 13:03:47 +0530

    Problem_1.js

---
+++ problem_solving/javascript/Problem_1.js
@@ -0,0 +1,46 @@
+
+//input
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+//reverseArray function 
+function reverseArray(a) {
+     a.reverse();
+     return a;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
