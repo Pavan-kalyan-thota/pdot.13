64b94f11745a5edd127b6a198f00d81341b3e688
Ravula Nishanth <nishanth.ravula@outlook.com>
2020-04-08 13:59:36 +0530

    Add files via upload

---
+++ problem_solving/javascript/problem_2.js
@@ -0,0 +1,74 @@
+//global variables
+let n = 0;
+let d = 0;
+let data = [];
+
+//read standard input
+process.stdin.setEncoding("utf8");
+process.stdin.resume();
+
+//store input
+let input = "";
+process.stdin.on("data", function(data) {
+  input += data;
+});
+process.stdin.on("end", function() {
+  let linesOfInput = input.split("\n");
+  let temp = linesOfInput[0].split(" ");
+  temp.map(Number);
+  n = temp[0];
+  d = temp[1];
+
+  data = linesOfInput[1].split(" ");
+
+  main();
+})
+
+//let's do it
+function main() {
+  //verify values
+  if (n <= 0 || d <= 0 || data.length > n || data.length < d) {
+    throw Error("The input values are invalid.");
+  }
+
+  //get result
+  //let result = getResultsUsingLoop(); in is case the code will not execute in given time limit.
+  let result = getResultUsingArrayMap();
+
+  //output
+  process.stdout.write(result.join(" "));
+  process.stdout.write("\n");
+}
+
+//Solution using for-loop 
+function getResultsUsingLoop() {
+  let leftRotate = function(result) {
+    let temp = [];
+    for (var i=0; i<n-1; i++) {
+      temp[i] = result[i+1];
+    }
+    temp[i] = result[0];
+    return temp;
+  };
+
+  let result = data.slice(0);
+  for (let i = 0; i < d; i++) {
+    result = leftRotate(result);
+  }
+
+  return result;
+}
+
+//Solution using array.map()
+function getResultUsingArrayMap() {
+  let result = data.map(function(value, index) {
+    let pos = parseInt(index) + parseInt(d );
+    if (pos > n-1) {
+      pos = pos - n;
+    }
+    return data[pos];
+  });
+
+  return result;
+}
+
